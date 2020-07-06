f946becceefbd93ece5d192c41e1b5a53a6a4e2a
Marcel Lambert <marcel.lambert@wesualize.com>
2020-07-06 10:33:54 +0200

    feat: add dummy gtk module

---
+++ src/widgets/gtk/mod.rs
@@ -0,0 +1,27 @@
+/*
+ * Copyright (2020) by Marcel Lambert.
+ * This project is dual licensed under either MIT or Apache-2.0.
+ */
+
+
+pub mod native;
+
+mod defs;
+
+mod button;
+mod root;
+mod window;
+mod event;
+mod menu;
+mod menu_item;
+
+pub mod error;
+
+pub use button::*;
+pub use root::*;
+pub use window::*;
+pub use defs::*;
+pub use menu::*;
+pub use menu_item::*;
+
+pub(self) mod utils;
