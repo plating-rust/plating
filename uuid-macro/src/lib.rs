d2febc71e1ded6ac524f46cfcd7a9c71db503df5
Marcel Lambert <marcel.lambert@wesualize.com>
2020-07-12 15:28:18 +0200

    docs: add docs (and doc tests) for noop!} macro

--- uuid-macro/src/lib.rs
+++ uuid-macro/src/lib.rs
@@ -23,7 +23,28 @@ use proc_macro_hack::proc_macro_hack;
 ///
 /// Uses [uuid](https://crates.io/crates/uuid) crate
 #[proc_macro_hack]
-pub use plating_uuid_macro_hack::{noop, uuid};
+pub use plating_uuid_macro_hack::uuid;
+
+/// Noop macro used internally by plating.
+///
+/// ```rust
+/// # use plating_macros::{noop};
+/// //not much to tell, the following line does nothing
+/// noop!();
+/// ```
+///
+/// # Why
+/// Used to typedef to macros when specific feature flags are disabled.
+/// ```rust
+/// #[cfg(feature = "log")]
+/// pub(crate) use log::{debug, error, info, trace, warn};
+/// #[cfg(not(feature = "log"))]
+/// pub(crate) use plating_macros::{
+///    noop as debug, noop as info, noop as warn, noop as error, noop as trace,
+/// };
+/// ```
+#[proc_macro_hack]
+pub use plating_uuid_macro_hack::noop;
 
 #[cfg(test)]
 mod tests {
